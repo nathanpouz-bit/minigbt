d7310217dec0d17735d2137cb5f5ee92a4a371dd
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 16:03:36 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
-import torch
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 MODEL_NAME = "microsoft/DialoGPT-medium"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
 model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
