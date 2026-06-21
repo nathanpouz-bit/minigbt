ac39bebd93a5717aee654881d61b8f7db604f9c0
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 16:35:11 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
 
 MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-model = AutoModelForCausalLM.from_pretrained(
-    MODEL_NAME
-)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 
 model.eval()
