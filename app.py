8cb58a967844216ee24f8af706211c4a3c98d2fa
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 16:41:04 +0200

    Integrate transformer model for message response
    Updated Streamlit app to use a transformer model for generating responses based on user input.

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-st.title("TEST STREAMLIT")
+st.title("Test modèle")
+
+MODEL_NAME = "distilgpt2"
+
+@st.cache_resource
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+    return tokenizer, model
+
+tokenizer, model = load_model()
 
 msg = st.text_input("Message")
 
-if st.button("Envoyer"):
-    st.write("Tu as écrit :", msg)
+if st.button("Envoyer") and msg:
+
+    inputs = tokenizer(msg, return_tensors="pt")
+
+    output = model.generate(
+        **inputs,
+        max_new_tokens=30
+    )
+
+    response = tokenizer.decode(
+        output[0],
+        skip_special_tokens=True
+    )
+
+    st.write(response)
