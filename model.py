c6f448addb7c89d86558916ee2ca2894e02a156d
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 16:55:59 +0200

    Change model from DialoGPT to TinyLlama

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-MODEL = "microsoft/DialoGPT-medium"
+MODEL = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForCausalLM.from_pretrained(MODEL)
