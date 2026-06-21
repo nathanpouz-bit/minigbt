ef80259215a327d4d9c3574e65f50897327e5ae6
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 17:12:12 +0200

    Refactor model loading in model.py
    Refactored model loading by removing caching and simplifying the load process.

--- model.py
+++ model.py
@@ -1,17 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-import streamlit as st
 
-MODEL = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+MODEL = "gpt2"
 
-@st.cache_resource
-def load_model():
-    tokenizer = AutoTokenizer.from_pretrained(MODEL)
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL,
-        low_cpu_mem_usage=True
-    )
-    model.eval()
-    return tokenizer, model
-
-tokenizer, model = load_model()
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForCausalLM.from_pretrained(MODEL)
+model.eval()
