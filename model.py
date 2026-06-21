37ead91e6bc2a9a325dcfeb8ac5a3c1b7e75e956
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 15:19:03 +0200

    Initialize model and tokenizer for DialoGPT

---
+++ model.py
@@ -0,0 +1,6 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+MODEL_NAME = "microsoft/DialoGPT-medium"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
