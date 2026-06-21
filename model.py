7ca2f6f0e7fc1ea496f7df28133b2b5adbddbb95
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 15:47:03 +0200

    Refactor model loading with dtype and device map

--- model.py
+++ model.py
@@ -3,4 +3,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 MODEL_NAME = "microsoft/DialoGPT-medium"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME,
+    torch_dtype="auto",
+    device_map="auto"
+)
