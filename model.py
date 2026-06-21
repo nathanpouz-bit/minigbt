3894310fae40d5acb761b30a4ed518ec71d567d4
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 16:49:59 +0200

    Change model from DialoGPT-medium to DialoGPT-large

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-MODEL = "microsoft/DialoGPT-medium"
+MODEL = "microsoft/DialoGPT-large"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForCausalLM.from_pretrained(MODEL)
